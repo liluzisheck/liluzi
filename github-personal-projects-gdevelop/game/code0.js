3d59ff67e44cfef620f32c075f033ded6d60a114
liluzisheck <i.raskaley@ortlyc.kyiv.ua>
2025-02-25 08:29:43 +0000

    перші зміни

---
+++ github-personal-projects-gdevelop/game/code0.js
@@ -0,0 +1,20 @@
+gdjs.Untitled_32sceneCode = {};
+gdjs.Untitled_32sceneCode.localVariables = [];
+
+
+gdjs.Untitled_32sceneCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Untitled_32sceneCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Untitled_32sceneCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['Untitled_32sceneCode'] = gdjs.Untitled_32sceneCode;
