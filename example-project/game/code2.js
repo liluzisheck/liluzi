89af7dfeb8eab0aee88aac991aca9bce35986c89
liluzisheck <i.raskaley@ortlyc.kyiv.ua>
2025-04-20 16:03:39 +0000

    4242352352

---
+++ example-project/game/code2.js
@@ -0,0 +1,20 @@
+gdjs.WinCode = {};
+gdjs.WinCode.localVariables = [];
+
+
+gdjs.WinCode.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.WinCode.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.WinCode.eventsList0(runtimeScene);
+
+
+return;
+
+}
+
+gdjs['WinCode'] = gdjs.WinCode;
